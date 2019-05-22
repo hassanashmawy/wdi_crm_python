a506b6ad7fe93622606b90fe8d9d63ac88812b10
hassanashmawy <hassan.ashmawy@gmail.com>
2019-05-22 11:32:36 -0400

    db!

--- crm.py
+++ crm.py
@@ -3,11 +3,11 @@ from contact import Contact
 class CRM:
 
   def main_menu(self):
-    while True: # repeat indefinitely
+    while True: 
       self.print_main_menu()
       user_selected = int(input())
       self.call_option(user_selected)
-  
+
   def print_main_menu(self):
     print('[1] Add a new contact')
     print('[2] Modify an existing contact')
@@ -16,8 +16,8 @@ class CRM:
     print('[5] Search by attribute')
     print('[6] Exit')
     print('Enter a number: ')
-  
-  
+
+
   def call_option(self, user_selected):
     if user_selected == 1:
       self.add_new_contact()
@@ -31,61 +31,64 @@ class CRM:
       self.search_by_attribute()
     elif user_selected == 6:
       exit()
-  # Finish off the rest for 3 through 6
-  # To be clear, the methods add_new_contact and modify_existing_contact
-  # don't do anything yet
-  
-  
+    
   def add_new_contact(self):
-    # get all the required info from our user:
     print('Enter First Name: ')
     first_name = input()
-
     print('Enter Last Name: ')
     last_name = input()
-
     print('Enter Email Address: ')
     email = input()
-
     print('Enter a Note: ')
     note = input()
-
-    Contact.create(first_name, last_name, email, note)
+    contact = Contact.create(
+      first_name=first_name,
+      last_name=last_name,
+      email=email,
+      note=note
+    )
   
   @classmethod
   def modify_existing_contact(cls):
-    print('Enter ID: ')
+    print('Please enter an id for the contact to be modified.')
     id = input()
-    contact = Contact.find(id)
-    print(contact)
-    print('please enter an attribute to change')
-    n_attribute = input()
-    print('please enter a new value')
-    n_value = input()
-    contact.update(n_attribute, n_value)
-    return contact.update
-    
-    
-
-  def delete_contact(self):
-    print('Please enter ID of contact to delete: ')
-    id = input()
-    contact = Contact.find(int(id))
-    contact.delete()
+    contact = Contact.get(id=id)
+    print('Please enter the attribute to be changed.')
+    update_attribute = input()
+    print('Please enter the new value for the attribute.')
+    update_value = input()
+    setattr(contact, update_attribute, update_value)
+    contact.save()
+    return contact
   
-   
+  def delete_contact(self):
+    print('Please enter an id for the contact to be deleted.')
+    id = input()
+    contact = Contact.get(id=id)
+    contact.delete_instance()
+  
   def display_all_contacts(self):
-    Contact.all()
+    for contact in Contact.select():
+      print(contact.full_name())
   
   def search_by_attribute(self):
-    print('Which attribute would you like to search by?: ')
+    print('Please enter the attribute you want to search by.')
     attribute = input()
-    print('Which value would you like to search by?: ')
+    print('Please enter the search term.')
     value = input()
-    show_contact = Contact.find_by(attribute, value)
-    print(show_contact)
-    
+    found_contact = None
+    if attribute == 'first_name':
+      found_contact = Contact.select().where(Contact.first_name == value)
+    elif attribute == 'last_name':
+      found_contact = Contact.select().where(Contact.last_name == value)
+    elif attribute == 'email':
+      found_contact = Contact.select().where(Contact.email == value)
+    elif attribute == 'note':
+      found_contact = Contact.select().where(Contact.note == value)
+    elif attribute == 'id':
+      found_contact = Contact.select().where(Contact.id == value)
+    for contact in found_contact:
+      print(contact)
 
 a_crm_app = CRM()
 a_crm_app.main_menu()
-
